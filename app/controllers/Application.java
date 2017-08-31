398fdc1dbaefab2814a53972418cf13136f154fc
wangbiao <wangbiao1027@126.com>
2017-08-31 22:25:29 +0800

    finish form

---
+++ app/controllers/Application.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.data.*;
+
+import views.html.index;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Application extends Controller {
+
+    public static Result index() {
+		List<String> lines = new ArrayList<String>();
+		lines.add("one");
+		lines.add("two");
+		lines.add("three");
+        return ok(views.html.index.render("Plat","Hello world Pass!",lines));
+    }
+	public static Result form(){
+		return ok(views.html.form.render());
+	}
+	
+	public static Result postForm(){
+		DynamicForm in = Form.form().bindFromRequest();
+		String result = in.get("content");
+		return ok(result);
+	}
+	
+    public static Result somePost(){
+    	return ok("you had post something!");
+    }
+    public static Result record(Long re){
+    	return ok("your record is : " + re);
+    }
+    public static Result newRecord(String name){
+    	return ok("the newRecord is:" + name);
+    }
+
+}
---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "Web"
+
+version := "1.0-SNAPSHOT"
+
+libraryDependencies ++= Seq(
+  javaJdbc,
+  javaEbean,
+  cache
+)     
+
+play.Project.playJavaSettings
